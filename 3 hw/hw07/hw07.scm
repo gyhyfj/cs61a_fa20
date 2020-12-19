a526d87050546c068d8efa895778298854ae107b
smileyflower <54049275+smileyflower@users.noreply.github.com>
2020-12-19 21:44:19 +0800

    Initial commit

---
+++ 3 hw/hw07/hw07.scm
@@ -0,0 +1,54 @@
+(define (filter-lst fn lst)
+  'YOUR-CODE-HERE
+  (cond((equal? (cdr lst) nil)(cond((fn (car lst)) lst)(else nil)))
+        (else (cond ((fn (car lst)) (cons (car lst) (filter-lst fn (cdr lst))))
+                    (else (filter-lst fn (cdr lst))))))
+  ;(cond ((null? lst) nil)
+  ;      ((fn (car lst)) (cons (car lst) (filter-lst fn (cdr lst))))
+  ;      (else (filter-lst fn (cdr lst))))
+)
+
+
+;;; Tests
+(define (even? x)
+  (= (modulo x 2) 0))
+(filter-lst even? '(0 1 1 2 3 5 8))
+; expect (0 2 8)
+
+
+(define (interleave first second)
+  'YOUR-CODE-HERE
+  ;(cond((or (null? first) (null? second)) (append first second))
+  ;      (else (cons (car first) (cons (car second) (interleave (cdr first) (cdr second)))))
+  ;)
+  (cond((null? first) second)
+      ((null? second) first)
+      (else (cons (car first) (cons (car second) (interleave (cdr first) (cdr second)))))
+  )
+)
+
+(interleave (list 1 3 5) (list 2 4 6))
+; expect (1 2 3 4 5 6)
+
+(interleave (list 1 3 5) nil)
+; expect (1 3 5)
+
+(interleave (list 1 3 5) (list 2 4))
+; expect (1 2 3 4 5)
+
+
+(define (accumulate combiner start n term)
+  'YOUR-CODE-HERE
+  (if (= n 0)
+    start
+    (accumulate combiner (combiner (term n) start) (- n 1) term))
+)
+
+
+(define (no-repeats lst)
+  'YOUR-CODE-HERE
+  (if (null? lst) lst
+    (cons (car lst)
+      (no-repeats (filter (lambda (x) (not (= (car lst) x))) (cdr lst)))))
+)
+
